c17048c0cd5c6577a84aa162b19296632aa47c6e
Dhruv <dhruvmalusare13@gmail.com>
2025-05-05 12:01:36 +0530

    added all files

---
+++ C_assignments/struct_ass.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+struct Student
+{
+    /* data */
+    int rno,marks;
+    char name[20];
+
+}s1[3];
+
+void main()
+{
+    for(int i=0;i<3;i++)
+    {
+        printf("\nEnter a Roll No:");
+        scanf("%d",&s1[i].rno);
+        printf("\nEnter a Name:");
+        scanf("%s",&s1[i].name);
+        printf("\nEnter a marks:");
+        scanf("%d",&s1[i].marks);
+
+    }
+
+    for(int i=0;i<3;i++)
+    {
+        printf("\n%d\n%s\n%d\n",s1[i].rno,s1[i].name,s1[i].marks);
+    }
+}
